1f93da7be11391241316f99da744c3985951ad7e
Gary Bhatti <mobileexpert1234@gmail.com>
2023-08-29 11:27:15 +0530

    Update AppConstants.kt
    log

--- app/src/main/java/com/colourchangedemo/util/AppConstants.kt
+++ app/src/main/java/com/colourchangedemo/util/AppConstants.kt
@@ -3,14 +3,13 @@ package com.colourchangedemo.util
 object AppConstants {
     const val BASE_URL="https://api.openai.com/v1/"
      const val CREATE_IMAGE="images/generations"
-    const val CREATE_CHAT="chat/completions"
+    const val CREATE_CHAT="chat/completions"\
     const val CREATE_MODERATION="moderations"
 
     /*param*/
     const val AUTHORIZATION="Authorization"
 
-    const val AUTHORIZATION_VALUE="Bearer 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"
-//    const val AUTHORIZATION_VALUE="Bearer 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"
+    const val AUTHORIZATION_VALUE="Bearer <ChatGPT key here>"
 
 
     const val IMAGE_MEDIA_TYPE="2"
